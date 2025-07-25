dbd8c9bfa6b0671a1cc39e4ea8da4a7fdf78228a
morning-start <morning-start@foxmail.com>
2025-07-25 22:30:50 +0800

    feat: 添加删除仓库时自动填写库名的功能
    - 支持 GitHub 和 Gitee 平台
    - 在删除仓库页面自动检测并填充库名
    - 优化用户操作流程，提高效率

---
+++ del_confirm.user.js
@@ -0,0 +1,69 @@
+// ==UserScript==
+// @name         Git Del Confirm
+// @version      2.1.0
+// @license      MIT
+// @description  删除仓库自动填写库名，支持 github、gitee
+// @author       Morning Start
+// @match        https://gitee.com/*
+// @match        https://github.com/*/settings
+// @icon         https://ts3.tc.mm.bing.net/th/id/ODF.KCStyvubJszELPE98QcMBA?w=32&h=32&qlt=90&pcl=fffffc&o=6&cb=thwsc4&pid=1.2
+// @namespace    morningstart.del-confirm
+// @updateURL    
+// ==/UserScript==
+
+(function () {
+    ("use strict");
+    const currentURL = window.location.href;
+
+    if (currentURL.includes("gitee.com")) {
+        giteeConfirm();
+    } else if (currentURL.includes("github.com")) {
+        githubConfirm();
+    }
+})();
+
+function githubConfirm() {
+    let del_btn = document.querySelector("#dialog-show-repo-delete-menu-dialog");
+
+    del_btn.addEventListener("click", function () {
+        const intervalId = setInterval(() => {
+            const ipt = document.querySelector("#verification_field");
+            const targetButton = document.getElementById('repo-delete-proceed-button');
+            if (targetButton && !ipt) {
+                // 若元素存在，点击该按钮
+                targetButton.click();
+                const button3 = document.getElementById('repo-delete-proceed-button');
+                const intervalCheck = setInterval(() => {
+                    if (button3.querySelector('.Button-label').textContent === "Delete this repository") {
+                        // 清除定时器，停止检查
+                        clearInterval(intervalId);
+                        clearInterval(intervalCheck);
+                        // 若按钮已禁用，解除禁用
+                        button3.disabled = false;
+                        // 填写库名
+                        let warp = document.querySelector('#repo-delete-proceed-button-container')
+                        let text = warp.querySelector('.FormControl-label')
+                        let input = warp.querySelector('#verification_field')
+                        const regex = /"([^"]*)"/;
+                        const match = text.textContent.match(regex);
+                        input.value = match[1]
+                    }
+                }, 200);
+            }
+        }, 500);
+        // console.log("button1");
+
+
+    });
+}
+
+function giteeConfirm() {
+    let del_btn = document.querySelector(".del-pro-btn");
+    del_btn.addEventListener("click", function () {
+        let wrap = document.querySelector(".del-or-clear-target-wrap");
+        wrap.querySelector(".ok").className = "ui button orange ok";
+        let text = wrap.querySelector(".highlight-black");
+        let input = wrap.querySelector("#path_with_namespace");
+        input.value = text.textContent;
+    });
+}
